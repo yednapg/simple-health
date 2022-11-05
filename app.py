e1226c207321a5541031f803ee3eb1aa3a896f8d
ravigargrud <ravigargrud@gmail.com>
2022-11-05 21:09:02 +0530

    up1

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home_page():
+    return render_template('index.html')
